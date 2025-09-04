b78ec2ca864b3aa0d122ec8f13ac9d60c7a64591
limafresh <173814432+limafresh@users.noreply.github.com>
2025-09-04 11:55:10 +0300

    Version 0.4, new customization options, dark Treeview theme if CustomTkinter mode is dark

--- CTkFileDialog/__init__.py
+++ CTkFileDialog/__init__.py
@@ -4,7 +4,7 @@ Author: limafresh
 Homepage: https://github.com/limafresh/CTkFileDialog
 """
 
-__version__ = "0.3"
+__version__ = "0.4"
 
 from .ctk_file_dialog import CTkFileDialog as CTkFileDialog
 from .ctk_file_widget import CTkFileWidget as CTkFileWidget
